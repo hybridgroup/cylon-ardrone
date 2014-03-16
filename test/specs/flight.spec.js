ce95d389f36ff77312539f97835bfde6a340a6ef
Andrew Stewart <andrew@stwrt.ca>
2014-03-16 12:39:55 -0700

    Improve test coverage for Flight driver

--- test/specs/flight.spec.js
+++ test/specs/flight.spec.js
@@ -1,32 +1,75 @@
 "use strict";
 
-var EventEmitter = require('events').EventEmitter,
-    Flight = source('flight');
+source('flight');
 
 describe('Cylon.Drivers.ARDrone.Flight', function() {
-  var driver = new Flight({ device: new EventEmitter });
+  var driver = new Cylon.Drivers.ARDrone.Flight({ device: {} });
 
-  it("provides a 'forward' function", function() {
-    expect(driver.forward).to.be.a('function');
+  driver.connection = {
+    animate: spy(),
+    front: spy(),
+    stop: spy()
+  };
+
+  describe("#commands", function() {
+    it("returns all ARDrone commands", function() {
+      var commands = driver.commands();
+      expect(commands).to.be.a('array');
+
+      for(var i = 0; i < commands.length; i++) {
+        expect(commands[i]).to.be.a('string');
+      };
+    });
   });
 
-  it("provides a 'frontFlip' function", function() {
-    expect(driver.frontFlip).to.be.a('function');
-  });
+  describe("proxies", function() {
+    describe("#hover", function() {
+      it("tells the drone to stop", function() {
+        driver.hover();
+        expect(driver.connection.stop).to.be.called;
+      });
+    });
 
-  it("provides a 'backFlip' function", function() {
-    expect(driver.backFlip).to.be.a('function');
-  });
+    describe("#forward", function() {
+      it("tells the drone to move forward", function() {
+        driver.forward(1);
+        expect(driver.connection.front).to.be.calledWith(1);
+      });
+    });
 
-  it("provides a 'leftFlip' function", function() {
-    expect(driver.leftFlip).to.be.a('function');
-  });
+    describe("#frontFlip", function() {
+      it("tells the drone to do a front flip", function() {
+        driver.frontFlip();
+        expect(driver.connection.animate).to.be.calledWith('flipAhead', 150);
+      });
+    });
 
-  it("provides a 'rightFlip' function", function() {
-    expect(driver.rightFlip).to.be.a('function');
-  });
+    describe("#backFlip", function() {
+      it("tells the drone to do a back flip", function() {
+        driver.backFlip();
+        expect(driver.connection.animate).to.be.calledWith('flipBehind', 150);
+      });
+    });
 
-  it("provides a 'wave' function", function() {
-    expect(driver.wave).to.be.a('function');
+    describe("#leftFlip", function() {
+      it("tells the drone to do a left flip", function() {
+        driver.leftFlip();
+        expect(driver.connection.animate).to.be.calledWith('flipLeft', 150);
+      });
+    });
+
+    describe("#rightFlip", function() {
+      it("tells the drone to do a right flip", function() {
+        driver.rightFlip();
+        expect(driver.connection.animate).to.be.calledWith('flipRight', 150);
+      });
+    });
+
+    describe("#wave", function() {
+      it("tells the drone to wave", function() {
+        driver.wave();
+        expect(driver.connection.animate).to.be.calledWith('wave', 750);
+      });
+    });
   });
 });
