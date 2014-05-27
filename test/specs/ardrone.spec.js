a69faea8614b50c72e9eb59baff32c6df876d1a8
Andrew Stewart <andrew@stwrt.ca>
2014-05-27 10:41:15 -0700

    Improve specs for ARDrone adaptor

--- test/specs/ardrone.spec.js
+++ test/specs/ardrone.spec.js
@@ -1,39 +1,86 @@
 "use strict";
 
-var EventEmitter = require('events').EventEmitter;
+var ARDrone = source('ardrone'),
+    Commands = source('commands');
 
-var ARDrone = source('ardrone');
+var LibARDrone = require('ar-drone');
 
 describe('Cylon.Adaptors.Ardrone', function() {
-  var adaptor = new ARDrone;
-
-  it("exposes a 'connect' method", function() {
-    expect(adaptor.connect).to.be.a('function');
-  });
-
-  it("exposes an array of drone commands", function() {
-    var commands = adaptor.commands();
-    expect(commands).to.be.an('array');
-
-    for (var i = 0; i < commands.length; i++) {
-      expect(commands[i]).to.be.a('string');
+  var drone = new ARDrone({
+    device: {},
+    connection: {
+      port: '127.0.0.1',
+      emit: spy()
     }
   });
 
-  it("defines adaptor events on the 'connect' method", function() {
-    stub(adaptor, 'defineAdaptorEvent');
-    adaptor.connection = new EventEmitter;
-    adaptor.connection.port = "192.168.1.1";
+  describe("constructor", function() {
+    it("sets @ardrone to null", function() {
+      expect(drone.ardrone).to.be.eql(null);
+    });
 
-    adaptor.connect(function() {});
+    it("sets @connector to null", function() {
+      expect(drone.connector).to.be.eql(null);
+    });
+  });
 
-    var events = [
-      'navdata', 'landing', 'landed', 'takeoff', 'hovering', 'flying',
-      'lowBattery', 'batteryChange', 'altitudeChange'
-    ];
+  describe("#commands", function() {
+    var commands = drone.commands();
 
-    for (var i = 0; i < events.length; i++) {
-      expect(adaptor.defineAdaptorEvent).to.be.calledWith({ eventName: events[i] });
-    }
+    it("is an array of strings", function() {
+      expect(commands).to.be.an('array');
+
+      commands.forEach(function(command) {
+        expect(command).to.be.a('string');
+      });
+    });
+
+    it("is equal to the Commands module", function() {
+      expect(commands).to.be.eql(Commands);
+    });
+  });
+
+  describe("#connect", function() {
+    var mockDrone = {},
+        callback = spy();
+
+    beforeEach(function() {
+      stub(LibARDrone, 'createClient').returns(mockDrone);
+
+      stub(drone, 'defineAdaptorEvent');
+      stub(drone, 'proxyMethods');
+
+      drone.connect(callback);
+    });
+
+    afterEach(function() {
+      LibARDrone.createClient.restore();
+
+      drone.defineAdaptorEvent.restore();
+      drone.proxyMethods.restore();
+    });
+
+    it("creates a new ARDrone instance", function() {
+      expect(LibARDrone.createClient).to.be.calledWith({ ip: '127.0.0.1'});
+    });
+
+    it("sets @connector to the returned ARDrone instance", function() {
+      expect(drone.connector).to.be.eql(mockDrone);
+    });
+
+    it("proxies the commands from the adaptor to the connector", function() {
+      expect(drone.proxyMethods).to.be.calledWith(Commands, mockDrone, drone);
+    });
+
+    it("defines adaptor events for the ARDrone", function() {
+      var events = [
+        'navdata', 'landing', 'landed', 'takeoff', 'hovering', 'flying',
+        'lowBattery', 'batteryChange', 'altitudeChange'
+      ];
+
+      events.forEach(function(e) {
+        expect(drone.defineAdaptorEvent).to.be.calledWith({ eventName: e });
+      });
+    });
   });
 });
